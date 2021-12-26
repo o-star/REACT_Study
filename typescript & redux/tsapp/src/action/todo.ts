12b21e78711fcb4da4da9e0a75141f43025ce16d
o-star <uh04049@naver.com>
2021-12-26 16:54:24 +0900

    redux toolkit createAction, createReducer code update

--- typescript & redux/tsapp/src/action/todo.ts
+++ typescript & redux/tsapp/src/action/todo.ts
@@ -1,15 +1,22 @@
-const ADD_TODO = 'TSAPP/todoReducer/ADD_TODO'
-const DELETE_TODO = 'TSAPP/todoReducer/ADD_TODO'
+import { createAction } from '@reduxjs/toolkit'
 
-const addTodo = (payload: string) =>
-  ({ type: ADD_TODO, payload: payload } as const)
-const deleteTodo = (payload: string) =>
-  ({
-    type: DELETE_TODO,
-    payload: payload,
-  } as const)
+const addTodo = createAction<string>('TSAPP/todoReducer/ADD_TODO')
+const deleteTodo = createAction<string>('TSAPP/todoReducer/ADD_TODO')
+//redux-toolkit createAction code
 
-export { ADD_TODO, DELETE_TODO, addTodo, deleteTodo }
+// const ADD_TODO = 'TSAPP/todoReducer/ADD_TODO'
+// const DELETE_TODO = 'TSAPP/todoReducer/ADD_TODO'
+
+// const addTodo = (payload: string) =>
+//   ({ type: ADD_TODO, payload: payload } as const)
+// const deleteTodo = (payload: string) =>
+//   ({
+//     type: DELETE_TODO,
+//     payload: payload,
+//   } as const)
+// original Action type, Action creator code
+
+export { addTodo, deleteTodo }
 export type todoType =
   | ReturnType<typeof addTodo>
   | ReturnType<typeof deleteTodo>
--- typescript & redux/tsapp/src/reducer/todoReducer.ts
+++ typescript & redux/tsapp/src/reducer/todoReducer.ts
@@ -1,15 +1,27 @@
+import { createReducer } from '@reduxjs/toolkit'
 import { todo } from '../action'
 
 const initState = ['soccer', 'report']
 
-export default function todoReducer(
-  state: string[] = initState,
-  action: todo.todoType
-) {
-  switch (action.type) {
-    case todo.ADD_TODO:
-      return [...state, action.payload]
-    default:
-      return state
-  }
-}
+const todoReducer = createReducer(initState, (builder) => {
+  builder
+    .addCase(todo.addTodo, (state, action) => {
+      state.push(action.payload)
+    })
+    .addDefaultCase((state, action) => {})
+})
+export default todoReducer
+//redux-toolkit createReducer code
+
+// export default function todoReducer(
+//   state: string[] = initState,
+//   action: todo.todoType
+// ) {
+//   switch (action.type) {
+//     case todo.addTodo.toString():
+//       return [...state, action.payload]
+//     default:
+//       return state
+//   }
+// }
+//original Reducer code
